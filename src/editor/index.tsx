7efdf63a3a66667e6131c6e52b7ed3c2850ef90d
changmen1 <zxl17339811909@163.com>
2026-01-20 14:18:30 +0800

    init

---
+++ src/editor/index.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+const LowcodeEditor: FC = () => {
+    return (
+        <>
+            <h1 className="text-8xl font-bold underline">
+                LowcodeEditor!
+            </h1>
+        </>
+    )
+}
+
+export default LowcodeEditor;
